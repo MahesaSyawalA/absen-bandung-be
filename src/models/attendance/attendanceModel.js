d17aa3f682f72425b5eb7a4527858617f619bd92
MahesaSyawalA <mahesaabd4@gmail.com>
2025-02-10 13:11:01 +0700

    adjustment : attendace param

--- src/models/attendance/attendanceModel.js
+++ src/models/attendance/attendanceModel.js
@@ -4,7 +4,7 @@ const Attendance = {
     add: async (date, status, user_id, photo_path, latitude, longtitude) => {
         try {
             const result = await client.query(
-                'INSERT INTO attendance (date, status, user_id, photo_path, latitude, longtitude) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+                'INSERT INTO attendance (date, status, user_id, photo_path, latitude, longitude) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
                 [date, status, user_id, photo_path, latitude, longtitude]
             );
             return result.rows[0];
--- src/utils/response.js
+++ src/utils/response.js
@@ -1,7 +1,7 @@
 // utils/response.js
 const response = (statusCode, data, message, res) => {
     res.status(statusCode).json({
-      status: statusCode === 200 ? 'success' : 'error',
+      status: statusCode === 200 || statusCode === 201  ? 'success' : 'error',
       data: data || null,
       message: message || null,
     });
